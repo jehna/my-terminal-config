88a1d95af26ce64e9f41b3bc73fa69c8386cf266
Jesse Luoto <jesse.luoto@gmail.com>
2019-04-09 13:45:52 +0300

    Add Android adb tool to PATH

---
+++ bash/android.bash
@@ -0,0 +1,2 @@
+# Add Android adb tool to PATH
+export PATH=~/Library/Android/sdk/platform-tools:$PATH
--- bash/main.bash
+++ bash/main.bash
@@ -1,2 +1,5 @@
 # Enable git autocompletion
 source $(dirname $BASH_SOURCE)/git-completion.bash
+
+# Android development specific configs
+source $(dirname $BASH_SOURCE)/android.bash

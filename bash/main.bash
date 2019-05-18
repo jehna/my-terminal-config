ef7c6a0d42d563ba72d149a7cba81a6ff4324046
Jesse Luoto <jesse.luoto@gmail.com>
2019-05-18 22:04:47 +0300

    Add NVM configs

--- bash/main.bash
+++ bash/main.bash
@@ -6,3 +6,6 @@ source $(dirname $BASH_SOURCE)/android.bash
 
 # Fastlane specific configs
 source $(dirname $BASH_SOURCE)/fastlane.bash
+
+# NVM specific configs
+source $(dirname $BASH_SOURCE)/nvm.bash

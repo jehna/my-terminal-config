bf77ab7f4ca8339704c6c7ab8608eab1a2d2b583
Jesse Luoto <jesse.luoto@gmail.com>
2020-03-19 16:44:18 +0200

    Fix gif conversion arguments

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -7,11 +7,11 @@
 #
 to_gif() {
     INPUT=$1
-    OUTPUT=$2
+    OUTPUT=${2:-${INPUT%.*}.gif}
 
     CONVERSION_FILTERS="paletteuse=bayer_scale=2:dither=none:diff_mode=rectangle"
 
-    ffmpeg -i $INPUT -vf palettegen=stats_mode=diff pipe:.png |
-      ffmpeg -i $INPUT -i pipe: -lavfi $CONVERSION_FILTERS -r 18 -gifflags +transdiff $OUTPUT
+    ffmpeg -i "$INPUT" -vf palettegen=stats_mode=diff pipe:.png |
+      ffmpeg -i "$INPUT" -i pipe: -lavfi $CONVERSION_FILTERS -r 18 -gifflags +transdiff "$OUTPUT"
 }
 

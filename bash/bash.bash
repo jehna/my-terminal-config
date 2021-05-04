7890064fb50017159e890abcf7ed504c2a83b563
Jesse Luoto <jesse.luoto@gmail.com>
2021-05-04 09:46:52 +0300

    Add git status visualization to bash prompt

--- bash/bash.bash
+++ bash/bash.bash
@@ -1,5 +1,7 @@
 # Remove Apple's nag of bash being deprecated
 export BASH_SILENCE_DEPRECATION_WARNING=1
 
-# Prompt that was cool in 2020
-PS1="\`if [ \$? = 0 ]; then echo 💎; else echo 💩; fi\`[\W]\\$ "
+# Bash prompt message
+VISUALIZE_GIT_UNSTAGED_CHANGES="\`if git status > /dev/null 2> /dev/null && ! git diff-files --quiet 2> /dev/null; then echo 🌱; fi\`"
+VISUALIZE_ERROR_CODE="\`if [ \$? = 0 ]; then echo 💎; else echo 💩; fi\`"
+PS1="${VISUALIZE_GIT_UNSTAGED_CHANGES}${VISUALIZE_ERROR_CODE}[\W]\\$ "

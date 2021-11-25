d81ba771e6a619e6efe4d6cad8dad76acf588257
Jesse Luoto <jesse.luoto@gmail.com>
2021-11-25 12:08:46 +0200

    Add one-off config for new mac

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -52,3 +52,10 @@ function monday() {
 # Keep mac alive for the execution of a task
 
 alias espresso="caffeinate -sm"
+
+# In case of a new Mac
+function configure_new_mac() {
+  # Makes sure that you keep connecting to correct beacon with multi beacon systems
+  # See: https://www.reddit.com/r/eero/comments/aom1ut/5x_speed_increase_on_macbook_pro_with_a_terminal/
+  sudo /System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport prefs joinMode=Strongest joinModeFallback=KeepLooking
+}

14825a1d16c25a21c3e432b6ef96fe7d9b941145
Jesse Luoto <jesse.luoto@gmail.com>
2024-08-29 11:43:59 +0300

    Fix missing argument error

--- git/aliases/squash.sh
+++ git/aliases/squash.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-COMMIT_HASH="$(git rev-parse "$1")"
-REST_OF_ARGS=("${@:2}")
-
-if [ -z "$COMMIT_HASH" ]; then
+if [ -z "$1" ]; then
   echo "Please provide a commit hash with 'git squash <commit-hash>'"
   exit 1
 fi;
+
+COMMIT_HASH="$(git rev-parse "$1")"
+REST_OF_ARGS=("${@:2}")
 
 GIT_EDITOR=: git commit  --no-edit --squash "$COMMIT_HASH" "${REST_OF_ARGS[@]}"
 GIT_EDITOR=: git rebase --autosquash --autostash -i "$COMMIT_HASH^"

3bc34fd4732312056015b20a74537135ac0a9ccc
Jesse Luoto <jesse.luoto@gmail.com>
2024-12-02 09:01:05 +0200

    Update main file to do automatic inclusion

--- bash/main.bash
+++ bash/main.bash
@@ -1,41 +1,11 @@
-# Bash configs
-source "$(dirname "$BASH_SOURCE")/bash.bash"
+# Get the directory of the current script
+DIR="$(dirname "${BASH_SOURCE[0]}")"
 
-# Enable git autocompletion
-source "$(dirname "$BASH_SOURCE")/git-completion.bash"
-
-# Android development specific configs
-source "$(dirname "$BASH_SOURCE")/android.bash"
-
-# Fastlane specific configs
-source "$(dirname "$BASH_SOURCE")/fastlane.bash"
-
-# NVM specific configs
-source "$(dirname "$BASH_SOURCE")/nvm.bash"
-
-# Ruby specific configs
-source "$(dirname "$BASH_SOURCE")/ruby.bash"
-
-# Aliases
-source "$(dirname "$BASH_SOURCE")/aliases.bash"
-
-# Homebrew
-source "$(dirname "$BASH_SOURCE")/homebrew.bash"
-
-# Enable gnupg
-source "$(dirname "$BASH_SOURCE")/gnupg.bash"
-
-# Flutter configs
-source "$(dirname "$BASH_SOURCE")/flutter.bash"
-
-# Python configs
-source "$(dirname "$BASH_SOURCE")/python.bash"
-
-# Cargo/Rust configs
-source "$(dirname "$BASH_SOURCE")/rust.bash"
-
-# npm autocomplete
-source "$(dirname "$BASH_SOURCE")/npm.bash"
-
-# Cursor
-source "$(dirname "$BASH_SOURCE")/cursor.bash"
+# Loop through all .bash files in the directory
+for file in "$DIR"/*.bash; do
+    # Exclude the main.bash file
+    if [[ "$file" != "$DIR/main.bash" ]]; then
+        # shellcheck disable=SC1090
+        source "$file"
+    fi
+done
